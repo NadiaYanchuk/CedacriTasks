80b406369b74d15395b7df0919af028be89e6602
Nadia <99792876+NadiaYanchuk@users.noreply.github.com>
2023-07-21 12:09:45 +0300

    Delete JavaScript/JavaScript/JavaScript directory

--- JavaScript/JavaScript/JavaScript/arrays/task3.js
+++
@@ -1,9 +0,0 @@
-function formatEmployees(employees) {
-    return employees.map(employee => ` Name: ${employee.name}, age: ${employee.age} years`);
-}
-const employers = [
-    { name: "John", age: 23 },
-    { name: "Jane", age: 33 },
-    { name: "Alex", age: 45 }
-];
-document.getElementById('task3').textContent = formatEmployees(employers);
--- JavaScript/JavaScript/JavaScript/reg_expressions/task2.js
+++
@@ -1,8 +0,0 @@
-function isValid(phone) {
-    let pattern = /^(\+4|3)(\s?\(\d{3}\)\s?|\s?)(\d{3}[-\s]?\d{2}[-\s]?\d{2})$/;
-    return pattern.test(phone);
-}
-function phone() {
-    let phone = document.getElementById("phone").value;
-    document.getElementById('reg_task2').innerHTML = (isValid(phone) ? "True" : "False");
-}
