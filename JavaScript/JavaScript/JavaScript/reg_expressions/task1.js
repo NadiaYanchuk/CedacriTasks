3212ee6dbebcea09a7d6681b8a8599d8749b9d72
NadiaYanchuk <nadiaianciuk@gmail.com>
2023-07-24 12:29:33 +0300

    Add new task: website by tutorial

--- JavaScript/JavaScript/JavaScript/reg_expressions/task1.js
+++
@@ -1,9 +0,0 @@
-function isValid(url) {
-    let start = /^(http:\/\/|https:\/\/)([\w-]+\.)+[\w-]+(\/[\w-./?%&=]*)?$/;
-    let end = /\.(php|html)$/;
-    return start.test(url) && end.test(url);
-}
-function url() {
-    let url = document.getElementById("url").value;
-    document.getElementById('reg_task1').innerHTML = "URL is " + (isValid(url) ? "valid" : "not valid");
-}
